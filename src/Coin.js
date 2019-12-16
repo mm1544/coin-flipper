a13753312eea1bd6c125b4e318bf47bcecfdd8f6
M. Minskis <martynas.minskis@gmail.com>
2019-12-16 19:17:12 +0000

    add coin.css

--- src/Coin.js
+++ src/Coin.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import './Coin.css'
 
 class Coin extends Component {
 
     render() {
         return(
-            <div>
+            <div className="Coin">
                 {this.props.flipped && (this.props.head ? <img src="https://www.fleur-de-coin.com/images/Thumbnails/euroimages-2eurosnew-SIZE250x250.jpg"></img>: <img src="https://www.fleur-de-coin.com/images/Thumbnails/euroimages-lithuania2euros-SIZE250x250.jpg"></img>)}
             </div>
         )
