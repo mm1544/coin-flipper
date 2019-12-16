12c35474c509a15c1227c316159a86b20552f56c
M. Minskis <martynas.minskis@gmail.com>
2019-12-16 18:46:15 +0000

    add some trivial changes

--- src/Coin.js
+++ src/Coin.js
@@ -5,7 +5,7 @@ class Coin extends Component {
     render() {
         return(
             <div>
-                {this.props.flipped ? (this.props.head ? <img src="https://www.fleur-de-coin.com/images/Thumbnails/euroimages-2eurosnew-SIZE250x250.jpg"></img>: <img src="https://www.fleur-de-coin.com/images/Thumbnails/euroimages-lithuania2euros-SIZE250x250.jpg"></img>) : ''}
+                {this.props.flipped && (this.props.head ? <img src="https://www.fleur-de-coin.com/images/Thumbnails/euroimages-2eurosnew-SIZE250x250.jpg"></img>: <img src="https://www.fleur-de-coin.com/images/Thumbnails/euroimages-lithuania2euros-SIZE250x250.jpg"></img>)}
             </div>
         )
     }
